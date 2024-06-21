e37e1ce9447106cd1a05c34b234156a13c88a742
freddy24-7 <freddy.austli@aol.com>
2024-06-21 18:30:33 +0200

    adjusting for performance

--- components/YoutubeSearch.tsx
+++ components/YoutubeSearch.tsx
@@ -1,11 +1,12 @@
 'use client';
 
-import React, { useState } from 'react';
+import React, { Suspense, useState } from 'react';
 import axios from 'axios';
-import YouTubeVideoList from './YouTubeVideoList';
-import YouTubeSearchForm from './YouTubeSearchForm';
-import YouTubeVideoInspiration from './YouTubeVideoInspiration';
 import Spinner from './Spinner';
+
+const YouTubeVideoList = React.lazy(() => import('./YouTubeVideoList'));
+const YouTubeSearchForm = React.lazy(() => import('./YouTubeSearchForm'));
+const YouTubeVideoInspiration = React.lazy(() => import('./YouTubeVideoInspiration'));
 
 const YoutubeSearch: React.FC = () => {
     const [searchTerm, setSearchTerm] = useState<string>('');
@@ -39,25 +40,17 @@ const YoutubeSearch: React.FC = () => {
 
     return (
         <div className="flex flex-col items-center w-full">
-            <YouTubeSearchForm
-                searchTerm={searchTerm}
-                setSearchTerm={setSearchTerm}
-                handleSearch={handleSearch}
-            />
-            {loading ? (
-                <Spinner loading={loading} />
-            ) : (
-                <div className="flex flex-col md:flex-row mt-4 w-full space-y-4 md:space-y-0 md:space-x-4">
-                    {!searchInitiated && (
-                        <div className="flex-grow md:w-2/3">
-                            <YouTubeVideoInspiration selectedVideoId={selectedVideoId} />
-                        </div>
-                    )}
-                    <div className={`w-full ${searchInitiated ? 'md:w-full' : 'md:w-1/3'} mt-4 md:mt-0`}>
-                        <YouTubeVideoList videos={searchResults} />
-                    </div>
-                </div>
-            )}
+            <Suspense fallback={<Spinner loading={true} />}>
+                <YouTubeSearchForm searchTerm={searchTerm} setSearchTerm={setSearchTerm} handleSearch={handleSearch} />
+                {!loading && !searchInitiated && (
+                    <YouTubeVideoInspiration selectedVideoId={selectedVideoId} />
+                )}
+                {loading ? (
+                    <Spinner loading={loading} />
+                ) : (
+                    <YouTubeVideoList videos={searchResults} />
+                )}
+            </Suspense>
         </div>
     );
 };
