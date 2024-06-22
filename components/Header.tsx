5166270e5f4e99da7b04de5f5daf7378c40f6a07
freddy24-7 <freddy.austli@aol.com>
2024-06-22 14:48:04 +0200

    Resolved merge conflict

--- components/Header.tsx
+++ components/Header.tsx
@@ -14,11 +14,7 @@ export const Header = () => {
     };
 
     return (
-<<<<<<< HEAD
-        <header className="bg-cover bg-center text-white p-4 sticky top-0 z-50 shadow-md flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
-=======
         <header className="bg-cover bg-center text-white p-4 sm:p-6 md:p-8 sticky top-0 z-50 shadow-md flex flex-col sm:flex-row justify-between items-center text-center sm:text-left">
->>>>>>> origin/03-Refactoring
             <div className="absolute inset-0 z-[-1]">
                 <Image
                     src="/audience.webp"
@@ -31,16 +27,6 @@ export const Header = () => {
             </div>
             <div className="flex flex-col sm:flex-row items-center gap-2 cursor-pointer" onClick={handleHomeClick}>
                 <div className="flex items-center gap-2">
-<<<<<<< HEAD
-                    <MonitorPlayIcon size={30} className="sm:size-50"/>
-                    <h1 className="text-base sm:text-xl font-bold">Play-lister</h1>
-                </div>
-                <h2 className="text-xs sm:text-base">Create your own playlist</h2>
-            </div>
-            <Button
-                onClick={handleHomeClick}
-                className="mt-2 sm:mt-0 bg-transparent hover:bg-white hover:text-orange-400 text-white font-bold py-2 px-3 sm:px-4 rounded transition-colors duration-300 flex items-center"
-=======
                     <MonitorPlayIcon size={40} className="sm:size-50"/>
                     <h1 className="text-lg sm:text-xl md:text-2xl font-bold">Play-lister</h1>
                 </div>
@@ -49,18 +35,13 @@ export const Header = () => {
             <Button
                 onClick={handleHomeClick}
                 className="mt-2 sm:mt-0 bg-transparent hover:bg-white hover:text-orange-400 text-white font-bold py-2 px-4 rounded transition-colors duration-300 flex items-center"
->>>>>>> origin/03-Refactoring
                 aria-label="Home"
                 role="button"
             >
-                <HomeIcon size={20} className="sm:size-50 mr-3" aria-hidden="true"/>
+                <HomeIcon size={24} className="sm:size-50" aria-hidden="true"/>
                 <span className="sr-only">Home</span>
             </Button>
-<<<<<<< HEAD
-            <LightDarkToggle className="fixed top-6 right-2"/>
-=======
             <LightDarkToggle className="fixed top-2 right-2"/>
->>>>>>> origin/03-Refactoring
         </header>
     );
 };
