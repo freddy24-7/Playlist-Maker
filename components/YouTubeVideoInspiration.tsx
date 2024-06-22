7dfbec1fbfdc6054229b328e8ed38938f84428b5
freddy24-7 <freddy.austli@aol.com>
2024-06-22 14:35:53 +0200

    adjusting for performance

--- components/YouTubeVideoInspiration.tsx
+++ components/YouTubeVideoInspiration.tsx
@@ -1,25 +1,15 @@
 'use client';
 
-<<<<<<< HEAD
-import React, { useEffect } from 'react';
-import YouTubeLazyPlayer from '@/components/YouTubeLazyPlayer'; // Import the lazy loading player
-import { useVideo } from '@/context/VideoContext';
-=======
 import React, { useEffect, useRef } from 'react';
 import YouTubePlainPlayer, { YouTubePlainPlayerRef } from './YouTubePlainPlayer';
->>>>>>> origin/03-Refactoring
 
 interface YouTubeVideoInspirationProps {
     selectedVideoId: string;
 }
 
 const YouTubeVideoInspiration: React.FC<YouTubeVideoInspirationProps> = ({ selectedVideoId }) => {
-<<<<<<< HEAD
-    const { videoId, setVideoId } = useVideo();
-=======
     const [videoId, setVideoId] = React.useState<string>('');
     const playerRef = useRef<YouTubePlainPlayerRef>(null);
->>>>>>> origin/03-Refactoring
 
     useEffect(() => {
         if (selectedVideoId) {
@@ -30,7 +20,7 @@ const YouTubeVideoInspiration: React.FC<YouTubeVideoInspirationProps> = ({ selec
     return (
         <div className="mt-4 order-2 md:order-none">
             {videoId ? (
-                <YouTubeLazyPlayer videoId={videoId} autoplay={true} />
+                <YouTubePlainPlayer ref={playerRef} videoId={videoId} />
             ) : (
                 <p>No video selected</p>
             )}
