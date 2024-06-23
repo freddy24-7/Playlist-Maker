8430bfb736e044b8dfa0f93e179e85379dda50f7
freddy24-7 <freddy.austli@aol.com>
2024-06-23 12:55:56 +0200

    refactoring

--- components/YouTubeLazyPlayer.tsx
+++ components/YouTubeLazyPlayer.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import useYouTubePlayer from '@/hooks/useYouTubePlayer';
-import YouTubeContainer from './YouTubeContainer';
+import YouTube, { YouTubeProps } from 'react-youtube';
+import Spinner from './Spinner';
 
 interface YouTubeLazyPlayerProps {
     videoId: string;
@@ -9,25 +9,45 @@ interface YouTubeLazyPlayerProps {
 
 const YouTubeLazyPlayer: React.FC<YouTubeLazyPlayerProps> = ({ videoId, autoplay = false }) => {
     const [showPlayer, setShowPlayer] = useState(false);
-    const { loading, onReady, opts } = useYouTubePlayer({ videoId, autoplay });
+    const [loading, setLoading] = useState(false);
+
+    const onPlayerReady: YouTubeProps['onReady'] = (event) => {
+        setLoading(false);
+        if (autoplay) {
+            event.target.playVideo();
+        }
+    };
+
+    const opts: YouTubeProps['opts'] = {
+        height: '390',
+        width: '640',
+        playerVars: {
+            autoplay: 1,  // This ensures video plays immediately when loaded
+            controls: 1,
+            enablejsapi: 1,
+            modestbranding: 1
+        },
+    };
 
     const handlePlayClick = () => {
+        setLoading(true);
         setShowPlayer(true);
     };
 
     return (
         <div className="relative">
+            {loading && (
+                <Spinner loading={true} />
+            )}
             {!showPlayer ? (
                 <div className="video-facade" onClick={handlePlayClick}>
-                    <img
-                        src={`https://img.youtube.com/vi/${videoId}/hqdefault.jpg`}
-                        alt="Video thumbnail"
-                        className="thumbnail"
-                    />
+                    <img src={`https://img.youtube.com/vi/${videoId}/hqdefault.jpg`} alt="Video thumbnail" />
                     <button className="play-button">Play</button>
                 </div>
             ) : (
-                <YouTubeContainer videoId={videoId} opts={opts} onReady={onReady} loading={loading} />
+                <div >
+                    <YouTube videoId={videoId} opts={opts} onReady={onPlayerReady} />
+                </div>
             )}
         </div>
     );
