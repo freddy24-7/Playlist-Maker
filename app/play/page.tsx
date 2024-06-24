e718e2b9d942e8112e9037b5448abcd50b40f2e9
freddy24-7 <freddy.austli@aol.com>
2024-06-24 16:26:21 +0200

    fetching in Server Components

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -1,26 +1,17 @@
-'use client';
-
-import React from 'react';
-import { useSearchParams } from 'next/navigation';
-import YouTubePlainPlayer from '@/components/YouTubePlainPlayer';
-import YouTubeContainer from "@/components/YouTubeContainer";
+import React, { Suspense } from 'react';
+import ClientPlayPage from '@/components/ClientPlayPage';
+import Spinner from '@/components/ui/Spinner';
 
 const PlayPage: React.FC = () => {
-    const searchParams = useSearchParams();
-    const videoId = searchParams.get('videoId');
-
     return (
         <div className="flex flex-col items-center justify-start min-h-screen pt-10 px-4 sm:px-6 lg:px-8 bg-background text-foreground">
             <div className="w-full max-w-3xl bg-card text-card-foreground p-4 rounded-lg">
-                {videoId ? (
-                    <YouTubePlainPlayer videoId={videoId} autoplay={true} />
-                ) : (
-                    <p>No video selected</p>
-                )}
+                <Suspense fallback={<Spinner loading={true} />}>
+                    <ClientPlayPage />
+                </Suspense>
             </div>
         </div>
     );
 };
 
 export default PlayPage;
-
