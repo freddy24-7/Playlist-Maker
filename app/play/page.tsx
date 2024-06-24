2fed49f18ee4a562756b03f594fd64b4a8096715
freddy24-7 <freddy.austli@aol.com>
2024-06-24 16:51:29 +0200

    fetching in Server Components

--- app/play/page.tsx
+++ app/play/page.tsx
@@ -5,7 +5,7 @@ import Spinner from '@/components/ui/Spinner';
 const PlayPage: React.FC = () => {
     return (
         <div className="flex flex-col items-center justify-start min-h-screen pt-10 px-4 sm:px-6 lg:px-8 bg-background text-foreground">
-            <div className="w-full max-w-3xl bg-card text-card-foreground p-4 rounded-lg">
+            <div className="w-full max-w-3xl bg-card text-card-foreground p-4 rounded-lg overflow-x-auto">
                 <Suspense fallback={<Spinner loading={true} />}>
                     <ClientPlayPage />
                 </Suspense>
